71efb4d1c0265f8df1df84e6ce5ed9ad9068fc3b
go661254 <gogomonkeykg@icloud.com>
2025-08-27 00:53:22 +0900

    fix: update main.py to use PORT from Render

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from main_routes import main_routes
 from calendar_routes import calendar_routes
+import os
 
 app = Flask(__name__)
 app.register_blueprint(main_routes)
 app.register_blueprint(calendar_routes)
 
-if __name__ == '__main__':
-    app.run(debug=False)
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
